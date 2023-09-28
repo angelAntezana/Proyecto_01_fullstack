70c9eb837a629c2bc2d10ec45a08984142646d1b
angelAntezana <angel2estevan@gmail.com>
2023-09-28 14:36:18 +0200

    Creado proyecto backend

---
+++ backend_01/src/test/java/com/angel/backend_01/Backend01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angel.backend_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
